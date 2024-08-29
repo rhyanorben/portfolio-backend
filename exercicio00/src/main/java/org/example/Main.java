1feacb30deb052c346efd4287452d04b0da7a27a
Rhyan Willemann Orben <rhyanorben@gmail.com>
2024-08-29 12:39:06 -0300

    Revisão de Conteúdo: Primeiro exercício em aula

--- exercicio00/src/main/java/org/example/Main.java
+++ exercicio00/src/main/java/org/example/Main.java
@@ -1,10 +1,35 @@
 package org.example;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        
+        String[] vetorPosicao = new String[4];
+        vetorPosicao[0] = "atacante";
+        vetorPosicao[1] = "meia";
+        vetorPosicao[2] = "zagueiro";
+        vetorPosicao[3] = "goleiro";
 
+        // Também é possível realizar dessa forma:
+        // String[] vetorPosicoes = {"atacante", "meia", "zagueiro", "goleiro"};
 
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Digite seu primeiro nome: ");
+        String primeiroNome = leitor.nextLine();
+
+        if (primeiroNome.isEmpty()) return;
+
+        System.out.println("Digite seu segundo nome: ");
+        String segundoNome = leitor.nextLine();
+
+        Random geradorAleatorio = new Random();
+        Integer numeroAleatorio = geradorAleatorio.nextInt(10, 100);
+
+        Integer indiceAleatorio = geradorAleatorio.nextInt(vetorPosicao.length);
+        String posicao = vetorPosicao[indiceAleatorio];
+
+        System.out.println(primeiroNome + segundoNome + numeroAleatorio + posicao);
     }
 }
