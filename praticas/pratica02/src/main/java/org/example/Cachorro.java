d2e47ad1b42e36c457b67aed1d3079e129ec1472
Rhyan Willemann Orben <rhyanorben@gmail.com>
2024-08-29 20:12:16 -0300

    Feito uma prática simples de POO

---
+++ praticas/pratica02/src/main/java/org/example/Cachorro.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Cachorro {
+
+    public String nome;
+    public String cor;
+
+    public void latir() {
+        System.out.println(this.nome + ": \"HOUH\"!");
+        }
+    }
---
+++ praticas/pratica02/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+    Cachorro dog = new Cachorro();
+    dog.nome = "Amora";
+    dog.latir();
+
+    }
+}
